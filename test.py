9d16e3fc82f3371cc0a33b75b60703df0b5d94e5
Julie <julieanneutrera05@gmail.com>
2024-08-02 17:41:44 +0800

    First commit

---
+++ test.py
@@ -0,0 +1,18 @@
+print('Hello, World!')
+
+number_one = 10
+number_two = 13
+
+first_name = 'Julie'
+
+char_sample = 'c'
+
+people_age = {
+    'Julie': 19,
+    'Hylla': 21,
+    'Andrea': 19
+}
+
+total = number_one + number_two
+
+print('The result is', total)
